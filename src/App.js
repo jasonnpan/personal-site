406679324c6ee02e05cb34c1b4d467f1e570b243
Jason Pan <pan.jason12@gmail.com>
2024-09-27 01:37:12 -0400

    added blob background + uploaded images

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import TrainerCard from "./components/TrainerCard";
 import ExperienceSection from "./components/ExperienceSection";
 import UnderConstructionCard from "./components/UnderConstructionCard";
+import BlobBackground from "./components/BlobBackground";
 
 function App() {
   return (
-    <>
-      <TrainerCard />
-      <ExperienceSection />
-      <UnderConstructionCard />
-    </>
+    <div class="relative min-h-screen overflow-hidden">
+      <BlobBackground />
+      <div class="relative z-10">
+        <TrainerCard />
+        <ExperienceSection />
+        <UnderConstructionCard />
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/BlobBackground.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Blob1 from "../assets/Blob1.png";
+
+function BlobBackground() {
+  return (
+    <div>
+      <img
+        src={Blob1}
+        alt="Blob 1"
+        class="absolute opacity-70 top-[-35vh] right-[-50vw] md:top-[-35vh] md:right-[-50vw] w-[120vw] h-[100vh] object-fit z-0 select-none pointer-events-none animate-pulse"
+        oncontextmenu="return false;"
+      />
+      <img
+        src={Blob1}
+        alt="Blob 1"
+        class="absolute top-[90vh] left-[-30vw] md:top-[30vh] md:left-[-30vw] w-[120vw] h-[120vh] object-fit z-0 select-none pointer-events-none animate-pulse"
+        oncontextmenu="return false;"
+      />
+      {/* <img
+        src={Blob1}
+        alt="Blob 1"
+        class="absolute opacity-70 top-[150vh] right-[-25vw] md:top-[100vh] md:right-[-30vw] w-[80vw] h-[80vh] object-fit z-0 select-none pointer-events-none animate-pulse"
+        oncontextmenu="return false;"
+      /> */}
+    </div>
+  );
+}
+
+export default BlobBackground;
